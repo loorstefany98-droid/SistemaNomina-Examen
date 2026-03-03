0bd30e8d6fe9c20cda574b360a2569ae0ab567ac
INES_ROCIO <quizhpiines@gmail.com>
2026-03-02 23:11:14 -0500

    Modificacion en la carpeta views y controllers por Rocio

---
+++ source/repos/ExamenIntegracion/app/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        { 
+            
+            return View();
+            
+        }
+
+        public IActionResult Registro()
+        {
+            
+            return View();
+
+        }
+        // POST
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "1234")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+            return View();
+        }
+    }
+}
---
+++ source/repos/ExamenIntegracion/app/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
+    }
+
+}
+
