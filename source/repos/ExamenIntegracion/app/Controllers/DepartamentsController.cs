f231cf7b7fbf19afa00ca2cb941cda3ab9ec1c8b
Alejandro Lopez <al79964@gmail.com>
2026-03-04 19:59:14 -0500

    correccion botones

--- source/repos/ExamenIntegracion/app/Controllers/DepartamentsController.cs
+++ source/repos/ExamenIntegracion/app/Controllers/DepartamentsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using app.Data;
 using app.Models;
+using app.ViewModels;
 using System.Linq;
 
 namespace app.Controllers
@@ -14,29 +15,27 @@ namespace app.Controllers
             _context = context;
         }
 
+        
         public IActionResult Index()
         {
             return RedirectToAction(nameof(DepartamentosLista));
         }
 
-        // LISTA (SELECT)
+        
         public IActionResult DepartamentosLista()
         {
-            var lista = _context.Departments
-                                .OrderBy(d => d.dept_no)
-                                .ToList();
-
-            return View(lista);
+            var lista = _context.Departments.OrderBy(d => d.dept_no).ToList();
+            return View(lista); 
         }
 
-        // GET CREATE
+        
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            return View(); 
         }
 
-        // POST CREATE (INSERT)
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Departments model)
@@ -47,24 +46,62 @@ namespace app.Controllers
                 return View(model);
             }
 
-            bool existe = _context.Departments.Any(d => d.dept_name == model.dept_name);
-            if (existe)
+            if (_context.Departments.Any(d => d.dept_name == model.dept_name))
             {
                 ModelState.AddModelError("dept_name", "Ese departamento ya existe.");
                 return View(model);
             }
 
-            // dept_no NO se asigna porque es IDENTITY
-            _context.Departments.Add(model);
+            _context.Departments.Add(model); 
             _context.SaveChanges();
 
             return RedirectToAction(nameof(DepartamentosLista));
         }
 
-        public IActionResult DepartamentosFormulario() => View();
-        public IActionResult AsignacionDepartamento() => View();
-        public IActionResult GerentedeDepartamento() => View();
+        
+        public IActionResult GerentedeDepartamento()
+        {
+            var lista = (from dm in _context.DeptManagers
+                         join d in _context.Departments on dm.dept_no equals d.dept_no
+                         join e in _context.Employees on dm.emp_no equals e.emp_no
+                         orderby d.dept_no
+                         select new GerenteRowVM
+                         {
+                             dept_no = d.dept_no,
+                             dept_name = d.dept_name,
+                             emp_no = e.emp_no,
+                             gerente = e.first_name + " " + e.last_name,
+                             from_date = dm.from_date,
+                             to_date = dm.to_date
+                         }).ToList();
+
+            return View(lista); 
+        }
+
+        
+        public IActionResult AsignacionDepartamento()
+        {
+            var lista = (from de in _context.DeptEmps
+                         join d in _context.Departments on de.dept_no equals d.dept_no
+                         join e in _context.Employees on de.emp_no equals e.emp_no
+                         orderby d.dept_no
+                         select new AsignacionRowVM
+                         {
+                             dept_no = d.dept_no,
+                             dept_name = d.dept_name,
+                             emp_no = e.emp_no,
+                             empleado = e.first_name + " " + e.last_name,
+                             from_date = de.from_date,
+                             to_date = de.to_date
+                         }).ToList();
+
+            return View(lista); 
+        }
+
+        
+        public IActionResult DepartamentosFormulario()
+        {
+            return View(); 
+        }
     }
 }
-
-
